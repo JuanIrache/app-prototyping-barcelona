575ef24a6074594ae22413531dbd40dc2a057600
JuanIrache <yrache@gmail.com>
2019-07-22 21:28:00 +0200

    tests and ideas

--- pages/about.js
+++ pages/about.js
@@ -2,14 +2,22 @@ import React from 'react';
 import axios from 'axios';
 
 const About = ({ user, repos }) => {
-  //   console.log(res);
+  console.log(user);
+
+  repos = repos.sort((a, b) => b.forks_count - a.forks_count);
+  repos = repos.sort((a, b) => b.stargazers_count - a.stargazers_count).slice(0, 10);
 
   return (
     <div>
       <div>{user.login + ' ' + user.public_repos}</div>
       <div>
         {repos.map(r => (
-          <p>{r.name + '-' + r.star_gazers}</p>
+          <div>
+            <p>
+              <a href={r.url}>{`${r.name} (${r.language}):`}</a> {`${r.description}`}
+            </p>
+            <p>{`stars: ${r.stargazers_count}| forks: ${r.forks_count}`}</p>
+          </div>
         ))}
       </div>
     </div>
@@ -32,12 +40,14 @@ About.getInitialProps = async () => {
       Accept: 'application/vnd.github.v3+json'
     }
   };
+  console.log('requesting');
+
   const { data: user } = await axios.get(
     `https://api.github.com/users/juanirache?client_id=${githubClientId}&client_secret=${githubClientSecret}`,
     config
   );
   const { data: repos } = await axios.get(
-    `https://api.github.com/users/juanirache/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`,
+    `https://api.github.com/users/juanirache/repos?per_page=100&client_id=${githubClientId}&client_secret=${githubClientSecret}`,
     config
   );
   return { user, repos };
---
+++ todo.md
@@ -0,0 +1,6 @@
+- - Portfolio vs app prototyping barcelona?
+- Filter projects by skill
+- Show most popular github repos through the api
+  Main section with portfolio / About with description and top repos / Why app prototyping with rationale
+
+github repo graph art!
