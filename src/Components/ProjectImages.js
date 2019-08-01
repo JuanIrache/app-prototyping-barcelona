8a64a6153c7b4737c86ec77177cfbe12d4b357d0
JuanIrache <yrache@gmail.com>
2019-08-02 01:58:15 +0200

    all thumbs apparently loading

--- src/Components/ProjectImages.js
+++ src/Components/ProjectImages.js
@@ -2,38 +2,22 @@ import React, { useContext, useState, useEffect } from 'react';
 import GalleryContext from '../contexts/GalleryContext';
 import '../style/ProjectImages.scss';
 
-const ProjectImages = ({ project, galleryImgs, title }) => {
+const ProjectImages = ({ project, galleryImgs }) => {
   const { setGallery, gallery } = useContext(GalleryContext);
   const regex = new RegExp(`/${project.id}\\d+\\.`);
-  const [images, setImages] = useState(
-    galleryImgs
-      .filter(img => regex.test(img))
-      .map(src => {
-        const img = new Image();
-        img.src = src;
-        return { src, img, loaded: img.naturalWidth !== 0 };
-      })
-  );
+  const [images, setImages] = useState(galleryImgs.filter(img => regex.test(img)).map(src => ({ src, loaded: false })));
 
   const handleSetGallery = idx => {
-    // setGallery({ ...gallery, selected: 1 });
-    // setImmediate(() => {
     setGallery({ ...gallery, selected: idx, visible: true });
-    // });
   };
 
   useEffect(() => {
-    const newImages = [];
-    let changes = false;
     images.forEach(image => {
-      if (image.loaded) newImages.push(image);
-      else if (image.img.naturalWidth !== 0) {
-        changes = true;
-        newImages.push({ src: image.src, loaded: true });
-      } else newImages.push(image);
+      const img = new Image();
+      img.onload = () => setImages(images.map(image => (img.naturalWidth !== 0 ? { ...image, loaded: true } : image)));
+      img.src = image.src;
     });
-    if (changes) setImages(newImages);
-  }, [images]);
+  }, []);
 
   return (
     <div className="ProjectImages">
@@ -46,7 +30,7 @@ const ProjectImages = ({ project, galleryImgs, title }) => {
               href="#!"
               className={`image${img.loaded ? ' visible' : ''}`}
               key={img.src}
-              alt={`${title} screenshot`}
+              alt={`${project.title} screenshot`}
               style={{ backgroundImage: `url(${img.src})` }}
               onClick={() => handleSetGallery(idx)}
             />
