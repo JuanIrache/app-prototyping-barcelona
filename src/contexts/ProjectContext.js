3ba9ce75be5211593ea1af8f9b015e0b60bbce2e
JuanIrache <yrache@gmail.com>
2019-08-01 23:47:33 +0200

    cleanup and todos

--- src/contexts/ProjectContext.js
+++ src/contexts/ProjectContext.js
@@ -10,8 +10,6 @@ export const ProjectContextProvider = ({ children }) => {
   const { tag } = useContext(TagContext);
 
   useEffect(() => {
-    console.log('project context tag', tag);
-
     if (tag.length) setProjects(initialProjects.filter(p => p.tags.includes(tag)).sort(() => Math.random() - 0.5));
     else setProjects(initialProjects);
   }, [tag]);
