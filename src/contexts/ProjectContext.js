1238dd02ee983a28bae50810d563f8c33fc2c26e
JuanIrache <yrache@gmail.com>
2019-08-01 23:46:27 +0200

    Fix filtering tags

--- src/contexts/ProjectContext.js
+++ src/contexts/ProjectContext.js
@@ -10,6 +10,8 @@ export const ProjectContextProvider = ({ children }) => {
   const { tag } = useContext(TagContext);
 
   useEffect(() => {
+    console.log('project context tag', tag);
+
     if (tag.length) setProjects(initialProjects.filter(p => p.tags.includes(tag)).sort(() => Math.random() - 0.5));
     else setProjects(initialProjects);
   }, [tag]);
