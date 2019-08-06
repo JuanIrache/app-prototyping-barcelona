92693ab9bae76705c6c676c387d9e2176f060685
JuanIrache <yrache@gmail.com>
2019-08-06 16:16:30 +0200

    fix selecting same tag again

--- src/Components/Tags.js
+++ src/Components/Tags.js
@@ -5,7 +5,7 @@ import ProjectContext from '../contexts/ProjectContext';
 import '../style/Tags.scss';
 
 const Projects = () => {
-  const { tag, tags, assignTag } = useContext(TagContext);
+  const { tag, tags, setTag } = useContext(TagContext);
   const { setInitial } = useContext(ProjectContext);
   const [showTitle, setShowTitle] = useState(false);
 
@@ -13,9 +13,13 @@ const Projects = () => {
     if (visible) setShowTitle(true);
   };
 
-  const handleAssignTag = tag => {
-    setInitial(0);
-    assignTag(tag);
+  const handleSetTag = e => {
+    let newTag = '';
+    if (e && e.target && e.target.name !== tag) newTag = e.target.name;
+    if (newTag !== tag) {
+      setInitial(0);
+      setTag(newTag);
+    }
   };
 
   return (
@@ -23,7 +27,7 @@ const Projects = () => {
       <div className="Tags">
         <p className={`separator${showTitle ? ' show' : ''}`}>
           Filter by tag or{' '}
-          <a href="#accordion" onClick={() => handleAssignTag('')}>
+          <a href="#accordion" onClick={() => handleSetTag('')}>
             see all projects
           </a>
         </p>
@@ -31,8 +35,12 @@ const Projects = () => {
           {tags.map(t => (
             <VisibilitySensor key={t} offset={{ bottom: 100 }}>
               {({ isVisible }) => (
-                <h4 className={`tag${t === tag ? ' active' : ''}${isVisible || showTitle ? ' show' : ''}`}>
-                  <a href="#accordion" name={t} onClick={handleAssignTag}>
+                <h4
+                  className={`tag${t === tag ? ' active' : ''}${
+                    isVisible || showTitle ? ' show' : ''
+                  }`}
+                >
+                  <a href="#accordion" name={t} onClick={handleSetTag}>
                     {t}
                   </a>
                 </h4>
--- src/contexts/TagContext.js
+++ src/contexts/TagContext.js
@@ -1,17 +1,15 @@
 import React, { createContext, useState } from 'react';
 import initialProjects from '../other/projects.json';
-const tags = initialProjects.reduce((acc, cur) => acc.concat(cur.tags.filter(t => !acc.includes(t))), []);
+const tags = initialProjects.reduce(
+  (acc, cur) => acc.concat(cur.tags.filter(t => !acc.includes(t))),
+  []
+);
 
 const TagContext = createContext();
 
 export const TagContextProvider = ({ children }) => {
   const [tag, setTag] = useState('');
-  const assignTag = e => {
-    let newTag = '';
-    if (e && e.target && e.target.name !== tag) newTag = e.target.name;
-    setTag(newTag);
-  };
-  return <TagContext.Provider value={{ tags, tag, assignTag }}>{children}</TagContext.Provider>;
+  return <TagContext.Provider value={{ tags, tag, setTag }}>{children}</TagContext.Provider>;
 };
 
 export default TagContext;
