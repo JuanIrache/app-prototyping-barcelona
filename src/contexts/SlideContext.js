60af23c351cbd44dfc8cf965c63743a5be2d6017
JuanIrache <yrache@gmail.com>
2019-08-06 16:25:00 +0200

    import from test branch

--- src/contexts/SlideContext.js
+++ src/contexts/SlideContext.js
@@ -1,9 +1,14 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import TagContext from './TagContext';
 
 const SlideContext = createContext();
 
 export const SlideContextProvider = ({ children }) => {
+  const { tag } = useContext(TagContext);
+
   const [slide, setSlide] = useState(0);
+
+  useEffect(() => setSlide(0), [tag]);
 
   return <SlideContext.Provider value={{ slide, setSlide }}>{children}</SlideContext.Provider>;
 };
